c5cda416e02c83fd34c574e56ef036eccc2ab681
Michael Noonan <michaelnoonan@gmail.com>
2016-05-09 10:55:35 +1000

    Investigate E2E tests.

---
+++ source/Octopus.Cli.E2ETests/CreateReleaseCommmandFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using Octopus.Cli.E2ETests.Util;
+using Octopus.Cli.Util;
+
+namespace Octopus.Cli.E2ETests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class CreateReleaseCommmandFixture
+    {
+        [Test]
+        public void PreventAutoChannelAndChannelArguments()
+        {
+            var octoexe = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().FullLocalPath()), "octo.exe");
+            var args = new StringBuilder("create-release --server=https://localhost:8085 --apiKey=API-123 --project OctoFX ");
+            var output = new StringBuilder();
+            var result = SilentProcessRunner.ExecuteCommand(octoexe, args.Append("--autochannel --channel=Default").ToString(), Environment.CurrentDirectory, info => output.AppendLine(info), error => output.AppendLine(error));
+            Approvals.Verify(output);
+        }
+    }
+}
---
+++ source/Octopus.Cli.E2ETests/Util/SilentProcessRunner.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace Octopus.Cli.E2ETests.Util
+{
+    public static class SilentProcessRunner
+    {
+        // ReSharper disable once InconsistentNaming
+        const int CP_OEMCP = 1;
+        static readonly Encoding oemEncoding;
+
+        static SilentProcessRunner()
+        {
+            try
+            {
+                CPINFOEX info;
+                if (GetCPInfoEx(CP_OEMCP, 0, out info))
+                {
+                    oemEncoding = Encoding.GetEncoding(info.CodePage);
+                }
+                else
+                {
+                    oemEncoding = Encoding.GetEncoding(850);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Couldn't get default OEM encoding: " + ex);
+                oemEncoding = Encoding.UTF8;
+            }
+        }
+
+        public static CmdResult ExecuteCommand(this CommandLineInvocation invocation)
+        {
+            return ExecuteCommand(invocation, Environment.CurrentDirectory);
+        }
+
+        public static CmdResult ExecuteCommand(this CommandLineInvocation invocation, string workingDirectory)
+        {
+            var arguments = (invocation.Arguments ?? "") + " " + (invocation.SystemArguments ?? "");
+            var infos = new List<string>();
+            var errors = new List<string>();
+
+            var exitCode = ExecuteCommand(
+                invocation.Executable,
+                arguments,
+                workingDirectory,
+                infos.Add,
+                errors.Add
+                );
+
+            return new CmdResult(exitCode, infos, errors);
+        }
+
+        public static int ExecuteCommand(string executable, string arguments, string workingDirectory, Action<string> output, Action<string> error)
+        {
+            return ExecuteCommand(executable, arguments, workingDirectory, output, error, CancellationToken.None);
+        }
+
+        public static int ExecuteCommand(string executable, string arguments, string workingDirectory, Action<string> output, Action<string> error, CancellationToken cancel)
+        {
+            try
+            {
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = executable;
+                    process.StartInfo.Arguments = arguments;
+                    process.StartInfo.WorkingDirectory = workingDirectory;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.StartInfo.StandardOutputEncoding = oemEncoding;
+                    process.StartInfo.StandardErrorEncoding = oemEncoding;
+
+                    using (var outputWaitHandle = new AutoResetEvent(false))
+                    using (var errorWaitHandle = new AutoResetEvent(false))
+                    {
+                        process.OutputDataReceived += (sender, e) =>
+                        {
+                            if (e.Data == null)
+                            {
+                                outputWaitHandle.Set();
+                            }
+                            else
+                            {
+                                output(e.Data);
+                            }
+                        };
+
+                        process.ErrorDataReceived += (sender, e) =>
+                        {
+                            if (e.Data == null)
+                            {
+                                errorWaitHandle.Set();
+                            }
+                            else
+                            {
+                                error(e.Data);
+                            }
+                        };
+
+                        process.Start();
+
+                        var running = true;
+
+                        cancel.Register(() =>
+                        {
+                            if (!running)
+                                return;
+                            DoOurBestToCleanUp(process);
+                        });
+
+                        if (cancel.IsCancellationRequested)
+                        {
+                            DoOurBestToCleanUp(process);
+                        }
+
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+
+                        process.WaitForExit();
+
+                        running = false;
+
+                        outputWaitHandle.WaitOne();
+                        errorWaitHandle.WaitOne();
+
+                        return process.ExitCode;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(string.Format("Error when attempting to execute {0}: {1}", executable, ex.Message), ex);
+            }
+        }
+
+        static void DoOurBestToCleanUp(Process process)
+        {
+            try
+            {
+                KillProcessAndChildren(process.Id);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    process.Kill();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetCPInfoEx([MarshalAs(UnmanagedType.U4)] int CodePage, [MarshalAs(UnmanagedType.U4)] int dwFlags, out CPINFOEX lpCPInfoEx);
+
+        const int MAX_DEFAULTCHAR = 2;
+        const int MAX_LEADBYTES = 12;
+        const int MAX_PATH = 260;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct CPINFOEX
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly int MaxCharSize;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = MAX_DEFAULTCHAR)]
+            public readonly byte[] DefaultChar;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = MAX_LEADBYTES)]
+            public readonly byte[] LeadBytes;
+
+            public readonly char UnicodeDefaultChar;
+
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly int CodePage;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_PATH)]
+            public readonly string CodePageName;
+        }
+
+        static void KillProcessAndChildren(int pid)
+        {
+            using (var searcher = new ManagementObjectSearcher("Select * From Win32_Process Where ParentProcessID=" + pid))
+            {
+                using (var moc = searcher.Get())
+                {
+                    foreach (var mo in moc.OfType<ManagementObject>())
+                    {
+                        KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+                    }
+                }
+            }
+
+            try
+            {
+                var proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                // Process already exited.
+            }
+        }
+    }
+}
