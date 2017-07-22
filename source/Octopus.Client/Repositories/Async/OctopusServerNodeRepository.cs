75c07d84190b9507ba0983baf5fa00a80d185eb7
Eddy Ma <eddy.ma@octopus.com>
2017-07-22 20:39:42 +1000

    Add new api to OctopusServerNodeRepository to get tasks

--- source/Octopus.Client/Repositories/Async/OctopusServerNodeRepository.cs
+++ source/Octopus.Client/Repositories/Async/OctopusServerNodeRepository.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Octopus.Client.Model;
 
 namespace Octopus.Client.Repositories.Async
 {
     public interface IOctopusServerNodeRepository : IModify<OctopusServerNodeResource>, IDelete<OctopusServerNodeResource>, IGet<OctopusServerNodeResource>, IFindByName<OctopusServerNodeResource>
     {
+        Task<IList<TaskResource>> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = null,
+            string taskState = null);
+
+        Task<int> GetTaskCount(OctopusServerNodeResource node, string taskState = null);
     }
 
     class OctopusServerNodeRepository : BasicRepository<OctopusServerNodeResource>, IOctopusServerNodeRepository
@@ -12,6 +18,17 @@ namespace Octopus.Client.Repositories.Async
         public OctopusServerNodeRepository(IOctopusAsyncClient client)
             : base(client, "OctopusServerNodes")
         {
+        }
+
+        public Task<IList<TaskResource>> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = null, string taskState = null)
+        {
+            return Client.Get<IList<TaskResource>>(node.Link("Tasks"), new { skip, take, taskState });
+        }
+
+        public async Task<int> GetTaskCount(OctopusServerNodeResource node, string taskState = null)
+        {
+            var result = await Client.Get<dynamic>(node.Link("TaskCount"), new { taskState });
+            return (int)result.Count;
         }
     }
 }
