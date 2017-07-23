0443df32f27c58a39d37813b15e9bdb31c3b64e5
Eddy Ma <eddy.ma@octopus.com>
2017-07-23 12:52:22 +1000

    code refactoring

--- source/Octopus.Client/Repositories/Async/OctopusServerNodeRepository.cs
+++ source/Octopus.Client/Repositories/Async/OctopusServerNodeRepository.cs
@@ -7,7 +7,7 @@ namespace Octopus.Client.Repositories.Async
 {
     public interface IOctopusServerNodeRepository : IModify<OctopusServerNodeResource>, IDelete<OctopusServerNodeResource>, IGet<OctopusServerNodeResource>, IFindByName<OctopusServerNodeResource>
     {
-        Task<List<TaskResource>> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = null,
+        Task<ResourceCollection<TaskResource>> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = null,
             string taskState = null);
 
         Task<int> GetTaskCount(OctopusServerNodeResource node, string taskState = null);
@@ -20,9 +20,9 @@ namespace Octopus.Client.Repositories.Async
         {
         }
 
-        public Task<List<TaskResource>> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = null, string taskState = null)
+        public Task<ResourceCollection<TaskResource>> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = null, string taskState = null)
         {
-            return Client.Get<List<TaskResource>>(node.Link("Tasks"), new { skip, take, taskState });
+            return Client.List<TaskResource>(node.Link("Tasks"), new {skip, take, taskState});
         }
 
         public async Task<int> GetTaskCount(OctopusServerNodeResource node, string taskState = null)
--- source/Octopus.Client/Repositories/OctopusServerNodeRepository.cs
+++ source/Octopus.Client/Repositories/OctopusServerNodeRepository.cs
@@ -1,12 +1,13 @@
 using System;
 using Octopus.Client.Model;
 using System.Collections.Generic;
+using Octopus.Client.Repositories.Async;
 
 namespace Octopus.Client.Repositories
 {
     public interface IOctopusServerNodeRepository : IModify<OctopusServerNodeResource>, IDelete<OctopusServerNodeResource>, IGet<OctopusServerNodeResource>, IFindByName<OctopusServerNodeResource>
     {
-        List<TaskResource> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = null,
+        ResourceCollection<TaskResource> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = null,
             string taskState = null);
 
         int GetTaskCount(OctopusServerNodeResource node, string taskState = null);
@@ -25,9 +26,9 @@ namespace Octopus.Client.Repositories
             return result.Count;
         }
 
-        public List<TaskResource> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = default(int?), string taskState = null)
+        public ResourceCollection<TaskResource> GetTasks(OctopusServerNodeResource node, int skip = 0, int? take = default(int?), string taskState = null)
         {
-            return Client.Get<List<TaskResource>>(node.Link("Tasks"), new { skip, take, taskState });
+            return Client.List<TaskResource>(node.Link("Tasks"), new { skip, take, taskState });
         }
     }
 }
