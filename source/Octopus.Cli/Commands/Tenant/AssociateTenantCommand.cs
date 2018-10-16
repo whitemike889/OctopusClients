7034cf587bf3c71586af72a30b33c213a94a3990
Matthew Casperson <matthewcasperson@gmail.com>
2018-10-16 11:26:35 +1000

    Added command to associate tenants to environments and projects

---
+++ source/Octopus.Cli/Commands/Tenant/AssociateTenantCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Octopus.Cli.Infrastructure;
+using Octopus.Cli.Repositories;
+using Octopus.Cli.Util;
+using Octopus.Client;
+
+namespace Octopus.Cli.Commands.Tenant
+{
+    [Command("associate-tenant", Description = "Associate a tenant with a project and environment")]
+    class AssociateTenantCommand : ApiCommand
+    {
+        public string TenantName { get; set; }
+        public string ProjectName { get; set; }
+        public string EnvironmentName { get; set; }
+
+        public AssociateTenantCommand(IOctopusClientFactory clientFactory, IOctopusAsyncRepositoryFactory repositoryFactory, IOctopusFileSystem fileSystem, ICommandOutputProvider commandOutputProvider) : base(clientFactory, repositoryFactory, fileSystem, commandOutputProvider)
+        {
+            var options = Options.For("Deletion");
+            options.Add("project=", "Name of the project", v => ProjectName = v);
+            options.Add("environment=", "Name of the environment.", v => EnvironmentName = v);
+            options.Add("tenant=", "Name of the tenant.", v => TenantName = v);
+        }
+
+        public async Task Request()
+        {
+            var features = await Repository.FeaturesConfiguration.GetFeaturesConfiguration();
+            if (features.IsMultiTenancyEnabled)
+            {
+                var tenant = await Repository.Tenants.FindByName(TenantName);
+                if (tenant == null)
+                {
+                    throw new CommandException($"Tenant $TenantName was not found");
+                }
+
+                var environment = await Repository.Environments.FindByName(EnvironmentName);
+                if (environment == null)
+                {
+                    throw new CommandException($"Environment $EnvironmentName was not found");
+                }
+
+                var project = await Repository.Projects.FindByName(ProjectName);
+                if (project == null)
+                {
+                    throw new CommandException($"Project $ProjectName was not found");
+                }
+
+                tenant.ConnectToProjectAndEnvironments(project, environment);
+            }
+            else
+            {
+                throw new CommandException("Multi-Tenancy is not enabled");
+            }
+        }
+    }
+}
